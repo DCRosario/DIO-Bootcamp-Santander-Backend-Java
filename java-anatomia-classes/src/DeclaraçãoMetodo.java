b0bedc0926b05ab429fc51649cbecf0a71d82c0f
DCRosario <drosario1807@gmail.com>
2023-08-28 13:15:15 -0300

    Aula 03 anatomia das classes DIO

---
+++ java-anatomia-classes/src/DeclaraçãoMetodo.java
@@ -0,0 +1,21 @@
+public class DeclaraçãoMetodo {
+
+    
+public static void main(String [] args) {
+
+    String afastamento = " ";
+    String primeiroNome = "Daniel";
+    String segundoNome = "Conceição";
+    String terceiroNome = "Rosario";
+
+    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome,afastamento,terceiroNome);
+
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome, String afastamento, String terceiroNome){
+
+    return "Resultado do método " + primeiroNome.concat(afastamento).concat(segundoNome).concat(afastamento).concat(terceiroNome);
+
+}
+}

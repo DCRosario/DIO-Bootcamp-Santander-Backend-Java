dde8c7be332930785fa0658e69d39954c37bc8b9
DCRosario <drosario1807@gmail.com>
2023-08-28 13:32:05 -0300

    Aula 04 anatomia das classes DIO

---
+++ java-anatomia-classes/src/IdentaçãoBoletim.java
@@ -0,0 +1,14 @@
+//Exemplo de codigo com boa identação.
+
+public class IdentaçãoBoletim {
+    public static void main(String [] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+        
+    }
+}

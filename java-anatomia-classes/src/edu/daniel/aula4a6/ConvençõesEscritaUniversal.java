b652dbf5b3bee67d6364352afd64e9e4bd292f7d
DCRosario <drosario1807@gmail.com>
2023-08-28 14:35:05 -0300

    Aula 06 anatomia das classes DIO

---
+++ java-anatomia-classes/src/edu/daniel/aula4a6/ConvençõesEscritaUniversal.java
@@ -0,0 +1,25 @@
+package edu.daniel.aula4a6;
+
+public class ConvençõesEscritaUniversal {
+    public static void main(String [] args) {
+        /*Escrita Não Recomendada de Variáveis
+         *  double salMedio = 1500.23   ||  variável abreviada, o que dificulta a compreensão
+         *  String emails = "aluno@escola.com"   ||  confuso se a variável seria um array ou único e-mail
+         *  String myName = "JOSEPH" ||  se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não 
+         *
+         * Escrita Recomendada de Variáveis
+         *  double salarioMedio = 1500.23;
+         *  String email = "aluno@escola.com";
+         *  String [] emails = {"aluno@escola.com", "professor@escola.com"};
+         *  String meunome = "JOSEPH";
+         * 
+         * Escrita de Métodos
+         *  somar(int n1, int n2){}
+         *  abrirConexao(){}
+         *  concluirProcessamento(){}
+         *  findById(int id){}  ||  não se assuste, você verá muito método em inglês em sua jornada
+         *  calcularImprimir(){}    ||  há algo de errado neste método, ele deveria ter uma única finalidade
+         */
+
+    }
+}
